f218f90b131aa46bb4cba455f3540319f5ec6861
reZach <reZach@users.noreply.github.com>
2021-02-18 21:24:48 -0600

    initial

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const crypto = require("crypto");
+
+export function cli(args){
+
+}
+
+// https://www.sohamkamani.com/nodejs/rsa-encryption/
+// https://medium.com/jspoint/creating-cli-executable-global-npm-module-5ef734febe32
+// https://www.twilio.com/blog/how-to-build-a-cli-with-node-js
+// https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e
+
+// https://build-system.fman.io/generating-license-keys
+// https://build-system.fman.io/manual/#license-keys
+
+// function sign(args){
+//     console.log(args);
+// }
+
+// module.exports = sign;
