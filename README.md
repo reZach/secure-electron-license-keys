2f9c46c495f56273f1dc6c86b6db97a23bc38b7a
reZach <reZach@users.noreply.github.com>
2021-02-19 23:00:35 -0600

    beta release

---
+++ README.md
@@ -0,0 +1,2 @@
+# secure-electron-license keys
+A secure way to implement offline license key validation in electron apps. 
--- src/index.js
+++ src/index.js
@@ -1,58 +1,106 @@
 import arg from "arg";
 const crypto = require("crypto");
+const fs = require("fs");
+const path = require("path");
 
 function parseArgumentsIntoOptions(rawArgs) {
     const args = arg({
-        "--secret": String,
-        "-s": "--secret"
+        "--major": String,
+        "--minor": String,
+        "--expire": String,
+        "--public": String,
+        "--private": String,
+        "--license": String,
+        "--output": String,
+        "-ma": "--major",
+        "-mi": "--minor",
+        "-e": "--expire",
+        "-pu": "--public",
+        "-pr": "--private",
+        "-l": "--license",
+        "-o": "--output"
+    }, {
+        permissive: false,
+        argv: rawArgs.slice(2),
+        stopAtPositional: false
     });
     return {
-        secret: args["--secret"] || ""
+        major: args["--major"] || "all",
+        minor: args["--minor"] || "all",
+        expire: args["--expire"] || "",
+        public: args["--public"] || "public.key",
+        private: args["--private"] || "private.key",
+        license: args["--license"] || "license.data",
+        output: args["--output"] || process.cwd()
     };
 }
+
+const cryptoKeyPairOptions = {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+        type: "spki",
+        format: "pem"
+    },
+    privateKeyEncoding: {
+        type: "pkcs8",
+        format: "pem"
+    }
+};
 
 export function cli(args) {
     const options = parseArgumentsIntoOptions(args);
     
-    
-    console.log(options);
+    // Define user license options/values
+    let userData = {
+        major: options.major,
+        minor: options.minor,
+        expire: options.expire
+    };
+
+    // Generate a public/private keypair
     const {
         publicKey,
         privateKey
-    } = crypto.generateKeyPairSync("rsa", {
-        modulusLength: 4096,
-        publicKeyEncoding: {
-            type: "spki",
-            format: "pem"
-        },
-        privateKeyEncoding: {
-            type: "pkcs8",
-            format: "pem",
-            cipher: "aes-256-cbc",
-            passphrase: ""
-        }
-    });
+    } = crypto.generateKeyPairSync("rsa", cryptoKeyPairOptions);
 
-    let encrypted = crypto.privateEncrypt(privateKey, Buffer.from(JSON.stringify({
-        a: 2
-    })));
-    let decrypted = crypto.publicDecrypt({
-        passphrase: ""   
-    }publicKey, encrypted);
+    // Sign user data with the private key
+    const encrypted = crypto.privateEncrypt(privateKey, Buffer.from(JSON.stringify(userData)));
 
-    console.log(decrypted);
+    // Save license data, along with public/private keys
+    const publicKeyFilePath = path.join(options.output, options.public);
+    const privateKeyFilePath = path.join(options.output, options.private);
+    const licenseFilePath = path.join(options.output, options.license);
+
+    console.log(`Saving public key file to '${publicKeyFilePath}'.`);
+    fs.writeFileSync(publicKeyFilePath, publicKey);
+
+    console.log(`Saving private key file to '${privateKeyFilePath}'.`);
+    fs.writeFileSync(privateKeyFilePath, privateKey);
+
+    console.log(`Saving license file to '${licenseFilePath}'.`);
+    fs.writeFileSync(licenseFilePath, encrypted);
 }
 
-// https://www.sohamkamani.com/nodejs/rsa-encryption/
-// https://medium.com/jspoint/creating-cli-executable-global-npm-module-5ef734febe32
-// https://www.twilio.com/blog/how-to-build-a-cli-with-node-js
-// https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e
+const validate = function(options) {    
+    let validationResult = {
+        _success: false
+    };
+    
+    // Retrieve public key and license data
+    const publicKey = fs.readFileSync(path.join(process.cwd(), "public.key"));
+    const licenseData = fs.readFileSync(path.join(process.cwd(), "license.data"));
 
-// https://build-system.fman.io/generating-license-keys
-// https://build-system.fman.io/manual/#license-keys
+    // Attempt to read license data with the public key    
+    try {
+        const decrypted = crypto.publicDecrypt(publicKey, licenseData);
 
-// function sign(args){
-//     console.log(args);
-// }
+        Object.assign(validationResult, JSON.parse(decrypted.toString("utf8")));
+        validationResult._success = true;
+    } catch (error) {
+        console.error(error);
+    }
 
-// module.exports = sign;
+    return validationResult;
+}
+
+export default validate;
